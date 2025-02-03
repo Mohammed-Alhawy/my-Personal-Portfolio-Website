5872ff36b41fc88f2aee01c09c72f374a4f54302
Mohammed <alhawymohammed2@gmail.com>
2025-02-03 02:41:55 +0200

    responsev

--- js/main.js
+++ js/main.js
@@ -27,12 +27,38 @@ opneMenu.addEventListener("click" , ()=>{
   if (menu.classList.contains('active-menu')) {
     opneMenu.innerHTML=`<i class="ri-close-large-fill"></i>`
     opneMenu.style.left="290px"
+
+    chaingeSkills()
+
   } else {
     opneMenu.innerHTML=`<i class="ri-menu-line"></i>`
     opneMenu.style.left="30px"
+    chaingeSkills2()
   }
 
 })
 
 
+
+
+
+
+const skillItem=document.querySelectorAll(".skill_item")
+
+
+function chaingeSkills() {
+
+  skillItem.forEach((item)=>{
+    item.classList.add("skillItem")
+  })
+
+}
+
+function chaingeSkills2() {
+
+  skillItem.forEach((item)=>{
+    item.classList.remove("skillItem")
+  })
+
+}
 
