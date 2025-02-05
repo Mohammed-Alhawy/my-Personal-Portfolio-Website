670f4729f8f87f1c843aae32886e2bac4a94d1e2
Mohammed <alhawymohammed2@gmail.com>
2025-02-05 14:47:34 +0200

    night mood

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
-// Active Link in side bar
-
-
-const All_links = document.querySelectorAll(".aside .nav a");
-console.log(All_links)
-
-All_links.forEach((Link) => {
-  Link.addEventListener("click", () => {
-    All_links.forEach((Link) => {
-      Link.classList.remove("Active");
-    });
-    Link.classList.add("Active");
-  });
-});
-
-
-
 
 const opneMenu=document.querySelector('.nav-toggler')
 const menu=document.querySelector('.aside')
 const mainContent=document.querySelector('.main-content')
 
 opneMenu.addEventListener("click" , ()=>{
+
   menu.classList.toggle("active-menu")
   mainContent.classList.toggle("main-content-active")
 
@@ -41,7 +25,7 @@ opneMenu.addEventListener("click" , ()=>{
 
 
 
-
+// skills respoensiv style
 
 const skillItem=document.querySelectorAll(".skill_item")
 
@@ -61,4 +45,63 @@ function chaingeSkills2() {
   })
 
 }
+
+
+
+
+
+
+
+// menu Active close and open wethn click on link
+
+// All_links.forEach(item =>{
+//   item.addEventListener("click" , ()=>{
+//     menu.classList.remove("active-menu")
+//     mainContent.classList.remove("main-content-active")
+//     opneMenu.innerHTML=`<i class="ri-menu-line"></i>`
+//     opneMenu.style.left="30px"
+//   })
+// })
+
+
+
+// Active Link in side bar with color
+
+const All_links = document.querySelectorAll(".aside .nav a");
+
+All_links.forEach((Link) => {
+
+  Link.addEventListener("click", () => {
+
+    All_links.forEach((Link) => {
+      Link.classList.remove("Active");
+      menu.classList.remove("active-menu")
+      mainContent.classList.remove("main-content-active")
+      opneMenu.innerHTML=`<i class="ri-menu-line"></i>`
+      opneMenu.style.left="30px"
+    });
+
+    Link.classList.add("Active");
+
+  });
+  
+});
+
+
+
+
+
+
+// =====******* typing animation ******====
+var typed = new Typed(".typing" , {
+  strings:["Ui / Ux Developer" , "Front End Developer"],
+  typedSpeed:350,
+  BackSpeed:250,
+  loop:true
+})
+
+
+
+
+// 
 
---
+++ js/style-switcher.js
@@ -0,0 +1,56 @@
+const styleSwitcherToggler=document.querySelector('.style-switcher-toggler')
+
+styleSwitcherToggler.addEventListener("click" , ()=>{
+  document.querySelector(".style-switcher").classList.toggle("open")
+})
+
+
+window.addEventListener("scroll" , ()=>{
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open")
+  }
+})
+
+
+// ============== theme colors =========
+
+const alternatStyle=document.querySelectorAll(".alternat-style")
+const allColors = document.querySelectorAll(".color")
+
+allColors.forEach( (color,index) =>{
+
+  color.addEventListener("click" , ()=>{
+    
+    alternatStyle.forEach(item =>{
+      item.setAttribute("disabled" , "disabled")
+    })
+    
+    alternatStyle[index].removeAttribute("disabled" , "disabled")
+
+  })
+
+} )
+
+
+
+
+// night mood and dark mood
+
+
+const dayNight=document.querySelector(".day-night")
+
+dayNight.addEventListener("click" , ()=>{
+  document.body.classList.toggle("dark")
+  dayNight.querySelector("i").classList.toggle("ri-sun-fill")
+  dayNight.querySelector("i").classList.toggle("ri-moon-fill")
+})
+
+
+window.addEventListener("load" , ()=>{
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("ri-sun-fill")
+  }else{
+    dayNight.querySelector("i").classList.add("ri-moon-fill")
+  }
+})
+
