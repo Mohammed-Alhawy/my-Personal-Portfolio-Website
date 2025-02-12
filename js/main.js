a42b8fafc42a79a23e0f4349e146cf7847098c58
Mohammed <alhawymohammed2@gmail.com>
2025-02-12 16:35:25 +0200

    finsh

--- js/main.js
+++ js/main.js
@@ -75,3 +75,27 @@ var typed = new Typed(".typing" , {
   loop:true
 })
 
+window.localStorage
+
+
+// /////////////
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".nav a[href*=" + sectionId + "]").classList.add("Active");
+    } else {
+      document.querySelector(".nav a[href*=" + sectionId + "]").classList.remove("Active");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
